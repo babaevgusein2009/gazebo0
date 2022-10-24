066cfe37d53a5a77bb77c09fdd3ef800e2cbff8b
babaevgusein2009 <114395968+babaevgusein2009@users.noreply.github.com>
2022-10-24 16:01:58 +0700

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #gittest
 git clone https://github.com/babaevgusein2009/gazebo0.git
-cd clover.gazebo0/
+cd gazebo0/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
+cd ..
