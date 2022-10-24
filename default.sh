cdff0415e29233327e17fdb875ec46fa8cf07063
babaevgusein2009 <114395968+babaevgusein2009@users.noreply.github.com>
2022-10-24 16:20:04 +0700

    Update default.sh

--- default.sh
+++ default.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/babaevgusein2009/gazebo0.git
 cd gazebo0/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
+cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
 cd ..
